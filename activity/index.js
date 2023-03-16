bc0511b7f480f1eb81a5188f0e8445ded1eddd7c
Gabby Dino <dino2030028@mls.ceu.edu.ph>
2023-03-16 09:08:09 +0800

    includes javascript intro activity

---
+++ activity/index.js
@@ -0,0 +1,21 @@
+function addNum(addNum1, addNum2){
+	result = addNum1 + addNum2;
+	console.log(result)
+	return result;
+}
+let sumResult = addNum(50, 5)
+
+function subtractNum(subNum1, subNum2){
+	result = subNum1 - subNum2;
+	console.log(result)
+	return result;
+}
+let differenceResult = subtractNum(10, 5)
+
+function multiplyNum(multNum1, multNum2){
+	result = multNum1 * multNum2;
+
+	return result;
+}
+let product = multiplyNum(69, 420)
+console.log(The result of the operation is: ${product})
